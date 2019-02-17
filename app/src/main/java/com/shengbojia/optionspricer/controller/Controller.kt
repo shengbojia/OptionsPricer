bf911915400b1674c009062514f15979f346ac96
Shengbo Jia <shengbojia@gmail.com>
2019-02-17 18:10:39 -0500

    made controller class to handle view to model communication

---
+++ app/src/main/java/com/shengbojia/optionspricer/controller/Controller.kt
@@ -0,0 +1,9 @@
+package com.shengbojia.optionspricer.controller
+
+import com.shengbojia.calculator.BlackScholesCalculator
+
+class Controller {
+    private val calculator = BlackScholesCalculator()
+
+
+}
--- app/src/main/java/com/shengbojia/optionspricer/ui/CalculatorFragment.kt
+++ app/src/main/java/com/shengbojia/optionspricer/ui/CalculatorFragment.kt
@@ -8,22 +8,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-import com.shengbojia.optionspricer.databinding.FragmentBlackScholesBinding
+import com.shengbojia.optionspricer.databinding.FragmentCalculatorBinding
 
 import com.shengbojia.optionspricer.R
+import com.shengbojia.optionspricer.controller.Controller
 
 /**
  * A simple [Fragment] subclass.
  *
  */
 class CalculatorFragment : Fragment() {
+    private val controller = Controller()
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?
     ): View? {
 
-        val binding = DataBindingUtil.inflate<FragmentBlackScholesBinding>(
+        val binding = DataBindingUtil.inflate<FragmentCalculatorBinding>(
             inflater, R.layout.fragment_calculator, container, false)
 
         return binding.root
