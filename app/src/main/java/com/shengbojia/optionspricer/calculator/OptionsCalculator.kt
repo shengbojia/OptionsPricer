df61fa0273589e59dd1ab39c72f1fdf1884f9c02
Shengbo Jia <shengbojia@gmail.com>
2019-02-18 21:12:44 -0500

    Woring error display

--- app/src/main/java/com/shengbojia/optionspricer/calculator/OptionsCalculator.kt
+++ app/src/main/java/com/shengbojia/optionspricer/calculator/OptionsCalculator.kt
@@ -1,7 +1,17 @@
 package com.shengbojia.optionspricer.calculator
 
+import android.util.Log
 import com.shengbojia.calculator.BlackScholesCalculator
+import java.math.BigDecimal
+import java.math.MathContext
 
 class OptionsCalculator : BlackScholesCalculator() {
 
+    fun percentToBigDecimal(percent: BigDecimal,
+                                    precision: MathContext = DEFAULT_PRECISION
+    ): BigDecimal {
+        Log.i("divide", "right before return")
+        return percent.divide(BigDecimal.TEN, DEFAULT_PRECISION)
+            .divide(BigDecimal.TEN, DEFAULT_PRECISION)
+    }
 }
--- app/src/main/java/com/shengbojia/optionspricer/ui/CalculatorFragment.kt
+++ app/src/main/java/com/shengbojia/optionspricer/ui/CalculatorFragment.kt
@@ -2,7 +2,6 @@ package com.shengbojia.optionspricer.ui
 
 import android.content.Context
 import android.databinding.DataBindingUtil
-import android.hardware.input.InputManager
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
@@ -10,24 +9,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
-import com.shengbojia.calculator.BlackScholesCalculator
+import android.widget.EditText
+import android.widget.TextView
 
 import com.shengbojia.optionspricer.databinding.FragmentCalculatorBinding
 
 import com.shengbojia.optionspricer.R
 import kotlinx.android.synthetic.main.fragment_calculator.*
-import com.shengbojia.calculator.BlackScholesCalculator.DEFAULT_PRECISION
-import java.lang.NumberFormatException
+import com.shengbojia.optionspricer.calculator.OptionsCalculator
 import java.math.BigDecimal
-import java.math.MathContext
 
 /**
  * A simple [Fragment] subclass.
  *
  */
 class CalculatorFragment : Fragment() {
-    private val calculator = BlackScholesCalculator()
+    private val calculator = OptionsCalculator()
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
@@ -46,7 +43,50 @@ class CalculatorFragment : Fragment() {
     }
 
     private fun calculateButtonClick(view: View) {
-        // TODO: Validate inputs as non empty
+
+        if (checkInputValidity()) {
+            calculate()
+        }
+
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun checkInputValidity(): Boolean {
+        return checkAndDisplayError(et_calc_stockinput, tv_calc_stockerror) and
+                checkAndDisplayError(et_calc_strikeinput, tv_calc_strikeerror) and
+                checkAndDisplayError(et_calc_timeinput, tv_calc_timeerror) and
+                checkAndDisplayError(et_calc_volinput, tv_calc_volerror) and
+                checkAndDisplayError(et_calc_rfinput, tv_calc_rferror)
+    }
+
+    private fun checkAndDisplayError(
+        input: EditText,
+        displayBox: TextView,
+        error: String = "This cannot be empty!"
+    ): Boolean {
+        if (!validInput(input)) {
+            displayBox.visibility = View.VISIBLE
+            displayError(displayBox, error)
+            return false
+        }
+        displayBox.visibility = View.INVISIBLE
+        return true
+    }
+
+    private fun validInput(input: EditText): Boolean {
+        return (input.text.toString()).isNotBlank()
+    }
+
+    private fun displayError(
+        displayBox: TextView,
+        error: String
+    ) {
+        displayBox.text = error
+    }
+
+    private fun calculate() {
+
         Log.i("divide", "right before conversions")
         val stockPrice: BigDecimal = et_calc_stockinput.text.toString().toBigDecimal()
         Log.i("divide", "right after conversions")
@@ -54,10 +94,10 @@ class CalculatorFragment : Fragment() {
         val maturity = et_calc_timeinput.text.toString().toBigDecimal()
         Log.i("divide", "big decimal conversions")
         var volatility = et_calc_volinput.text.toString().toBigDecimal()
-        volatility = percentToBigDecimal(volatility)
+        volatility = calculator.percentToBigDecimal(volatility)
         Log.i("divide", "right after volaitility conversion")
         var riskFree = et_calc_rfinput.text.toString().toBigDecimal()
-        riskFree = percentToBigDecimal(riskFree)
+        riskFree = calculator.percentToBigDecimal(riskFree)
         Log.i("divide", "right after riskfree conversion")
 
         var callPrice = calculator.callPricing(
@@ -81,17 +121,5 @@ class CalculatorFragment : Fragment() {
         tv_calc_cprice.text = callPrice.toDouble().toString()
         tv_calc_pprice.text = putPrice.toDouble().toString()
         gridlayout_calc_output.visibility = View.VISIBLE
-
-        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
-    }
-
-
-    private fun percentToBigDecimal(percent: BigDecimal,
-                                    precision: MathContext = DEFAULT_PRECISION
-    ): BigDecimal {
-        Log.i("divide", "right before return")
-        return percent.divide(BigDecimal.TEN, DEFAULT_PRECISION)
-            .divide(BigDecimal.TEN, DEFAULT_PRECISION)
     }
 }
