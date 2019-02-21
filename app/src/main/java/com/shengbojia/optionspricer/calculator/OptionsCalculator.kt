7e152b45af64d8d0c3f0f8a728c3d09e566d6264
Shengbo Jia <shengbojia@gmail.com>
2019-02-20 22:01:08 -0500

    added greeks

--- app/src/main/java/com/shengbojia/optionspricer/calculator/OptionsCalculator.kt
+++ app/src/main/java/com/shengbojia/optionspricer/calculator/OptionsCalculator.kt
@@ -27,6 +27,56 @@ class OptionsCalculator : BlackScholesCalculator() {
         )
     }
 
+    fun cDelta(): BigDecimal {
+        return super.callDelta(
+            inputData.stock,
+            inputData.strike,
+            inputData.maturity,
+            inputData.volatility,
+            inputData.riskFree
+        )
+    }
+
+    fun cGamma(): BigDecimal {
+        return super.callGamma(
+            inputData.stock,
+            inputData.strike,
+            inputData.maturity,
+            inputData.volatility,
+            inputData.riskFree
+        )
+    }
+
+    fun cVega(): BigDecimal {
+        return super.callVega(
+            inputData.stock,
+            inputData.strike,
+            inputData.maturity,
+            inputData.volatility,
+            inputData.riskFree
+        )
+    }
+
+    fun cTheta(): BigDecimal {
+        return super.callTheta(
+            inputData.stock,
+            inputData.strike,
+            inputData.maturity,
+            inputData.volatility,
+            inputData.riskFree
+        )
+    }
+
+    fun cRho(): BigDecimal {
+        return super.callRho(
+            inputData.stock,
+            inputData.strike,
+            inputData.maturity,
+            inputData.volatility,
+            inputData.riskFree
+        )
+    }
+
     fun putPrice(): BigDecimal {
         return super.putPricing(
             inputData.stock,
@@ -37,4 +87,53 @@ class OptionsCalculator : BlackScholesCalculator() {
         )
     }
 
+    fun pDelta(): BigDecimal {
+        return super.putDelta(
+            inputData.stock,
+            inputData.strike,
+            inputData.maturity,
+            inputData.volatility,
+            inputData.riskFree
+        )
+    }
+
+    fun pGamma(): BigDecimal {
+        return super.putGamma(
+            inputData.stock,
+            inputData.strike,
+            inputData.maturity,
+            inputData.volatility,
+            inputData.riskFree
+        )
+    }
+
+    fun pVega(): BigDecimal {
+        return super.putVega(
+            inputData.stock,
+            inputData.strike,
+            inputData.maturity,
+            inputData.volatility,
+            inputData.riskFree
+        )
+    }
+
+    fun pTheta(): BigDecimal {
+        return super.putTheta(
+            inputData.stock,
+            inputData.strike,
+            inputData.maturity,
+            inputData.volatility,
+            inputData.riskFree
+        )
+    }
+
+    fun pRho(): BigDecimal {
+        return super.putRho(
+            inputData.stock,
+            inputData.strike,
+            inputData.maturity,
+            inputData.volatility,
+            inputData.riskFree
+        )
+    }
 }
